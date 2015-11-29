fe9e9531e40895afe2d90b09ae2caf770e2e9771
Maxim Korytin <maximkorytin@iMac-Maxim.local>
2015-11-29 11:00:21 +0300

    Реализована инициализация пользователя

--- app/models/user.mysql.server.model.js
+++ app/models/user.mysql.server.model.js
@@ -2,7 +2,7 @@
 	'use strict';
 	// Подключаем зависимости
 	var crypto = require('crypto');
-    //var async = require('async');
+    var async = require('async');
     var contract = require('../lib/contract.js');
     var connectionProvider = require('../../config/database/mysql/mysql-connection-sting-provider.js');
     var connection = connectionProvider.mysqlConnectionProvider.getMysqlConnection();
@@ -13,29 +13,25 @@
     // Класс для работы с пользователями
 
     // Конструктор
-    function User(newUser){
-        //console.log(arguments);
-        // contract(arguments)
-        // .params('object')
-        // .end();
+    function User(userObj){     
 
         // Инициализация пользователя
-        if(newUser === undefined){ newUser = {}; }    	
+        if(userObj === undefined){ userObj = {}; }    	
     	
     	// Публичные свойства
-    	this.surname = newUser.surname;  // Фамилия пользователя
-        this.name = newUser.name; // Имя пользователя
-        this.lastname = newUser.lastname; // Отчество пользователя
-        this.email = newUser.email; // Электронный адрес пользователя
-        this.login = newUser.login; // Логин пользователя        
-        this.hashedPassword = ''; // Шифрованный пароль пользователя
-        this.salt = ''; // Соль для пароля
+    	this.surname = userObj.surname;  // Фамилия пользователя
+        this.name = userObj.name; // Имя пользователя
+        this.lastname = userObj.lastname; // Отчество пользователя
+        this.email = userObj.email; // Электронный адрес пользователя
+        this.login = userObj.login; // Логин пользователя        
+        this.hashedPassword = (userObj.hashedPassword) ? userObj.hashedPassword : ''; // Шифрованный пароль пользователя
+        this.salt = (userObj.salt) ? userObj.salt : ''; // Соль для пароля
         this.created = moment(new Date()).toDate(); // Дата создания
-        this.state = (newUser.state) ? newUser.state : '0'; // Состояние учетной записи   
+        this.state = (userObj.state) ? userObj.state : '0'; // Состояние учетной записи   
 
         // Закрытые свойства
-        if(newUser.password){
-            var password = newUser.password;
+        if(userObj.password){
+            var password = userObj.password;
             this.salt = Math.random() + ''; 
             this.hashedPassword = this._encryptPassword(password);
         }             
@@ -44,43 +40,11 @@
     //Методы класса
 
     // Закрытые методы класса
+    // Хэширование пароля
     User.prototype._encryptPassword = function(password){
         return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
     };
-
-
-
-    // Публичные методы
-    // Создать нового пользователя
-    User.prototype.save = function(callback){
-        // Инициализация переменных
-        var User = this;
-
-        // Проверяем переменные
-        if(this.surname && this.email && 
-           this.login && this.hashedPassword && this.salt){
-            
-            // Формируем запрос на создание пользователя
-            var insertStatement = "INSERT INTO `amidocs`.`users` SET?";
-            
-
-            if(connection){
-                connection.query(insertStatement, User, function(err, result){
-                    if(err) { throw err;}
-
-                    callback({status: 'success'});
-                    console.log(result);
-
-                });
-            }
-            callback(User);
-
-        }else{
-            console.log('Ошибка: не все данные заполнены.');
-            callback(false);
-        }
-    };
-
+    
     // Проверить на уникальность Логин пользователя
     User.prototype._checkUniqLogin = function(login, callback){
 
@@ -93,14 +57,39 @@
                 callback(true);
             }
         });
-    };
+    };    
+    
+    // Публичные методы
+    // Создать нового пользователя
+    User.prototype.save = function(callback){
+        // Инициализация переменных
+        var User = this;
 
+        // Проверяем переменные
+        if(this.surname && this.email && 
+           this.login && this.hashedPassword && this.salt){
+            
+            // Формируем запрос на создание пользователя
+            var insertStatement = "INSERT INTO `amidocs`.`users` SET?";            
+
+            if(connection){
+                connection.query(insertStatement, User, function(err, result){
+                    if(err) { throw err;}
+                    callback({status: 'success'});
+                    //console.log(result);                    
+                });
+            }
+            callback(User);
+        }else{
+            console.log('Ошибка: не все данные заполнены.');
+            callback(false);
+        }
+    }; 
 
     // Получить пользователя по Логину
-    User.prototype.getUserByLogin = function(login, callback){
+    User.getUserByLogin = function(login, callback){
         // Инициализвация переменных
         //var self = this;
-
         var selectStatement = "SELECT * FROM amidocs.users AS t1 WHERE t1.login = '"+login+"';";
         
         if(connection){
@@ -115,40 +104,42 @@
             });
         }        
     };
-
-
-
-    // Получить пользователя по ID
-    User.prototype.getUserById = function(userID, callback){
-            callback('User = ' + userID);
+    
+    // Авторизация пользователя
+    User.authorize = function(login, password, callback){
+        
+        // Инициализация переменных                
+        async.waterfall([function(callback){
+            // Получаем пользователя по логину
+            User.getUserByLogin(login, function(user){
+                
+                if(user){
+                    callback(null, user[0]);    
+                }else{
+                    callback({error: 'Пользователь не существует!'});
+                }
+                
+            }); 
+        },
+        function(user, callback){
+            if(user){
+                user = new User(user);
+                
+                console.log(user);
+               
+                   
+            }            
+        }], 
+        function(err, results){
+            if(err){
+                console.log(err);
+            }
+        });
     };
+    
+    //////////////////////////
 
-
-    var newUser = new User({surname: 'Аленин',
-                            name: 'Егор',
-                            lastname: 'Сергеевич',
-                            email: 'sliva666@mail.ru',
-                            login: 'Gosha',
-                            password: '123456',
-                            state: true,
-                            });  
-    console.log(newUser);
-    console.log("///////////////////");
-
-    var newUser2 = new User();  
-    console.log(newUser2);
-    console.log("///////////////////");
-
-
-    // newUser.getUserByLogin('PuNKeR', function(result){
-    //     console.log(result);
-    // });
-    newUser._checkUniqLogin("PuNKeR", function(result){
-        console.log(result);        
-    });   
-
-    // newUser.save(function(result){
-    //     console.log(result);
-    // });
-
+    User.authorize("PuNKeR", "123", function(result){
+        
+    });
 })();
